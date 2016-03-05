cab27804dc1e3a4d467d04294387760f4bdc48d7
Anders Wind <awia00@gmail.com>
2016-03-05 18:05:11 +0100

    Set up the entire game logic. Now one can play against a random computer

--- Projects/Project1/Connect4/src/iaip_c4/GameLogic.java
+++ Projects/Project1/Connect4/src/iaip_c4/GameLogic.java
@@ -6,6 +6,7 @@ public class GameLogic implements IGameLogic {
     private int x = 0;
     private int y = 0;
     private int playerID;
+    int[][] gameState;
     
     public GameLogic() {
         //TODO Write your implementation for this method
@@ -15,22 +16,82 @@ public class GameLogic implements IGameLogic {
         this.x = x;
         this.y = y;
         this.playerID = playerID;
+        gameState = new int[x][y];
         //TODO Write your implementation for this method
     }
 	
     public Winner gameFinished() {
-        //TODO Write your implementation for this method
-        return Winner.NOT_FINISHED;
+        for (int i = 0; i < gameState.length; i++)
+        {
+            for (int j = 0; j < gameState[i].length; j++)
+            {
+                if(CheckWin(i,j,1)) return Winner.PLAYER1;
+                if(CheckWin(i,j,2)) return Winner.PLAYER2;
+            }
+        }
+        for (int i = 0; i < gameState.length ;i++)
+        {
+            if(getEmptyRowOnColumn(i) != -1) return Winner.NOT_FINISHED;
+        }
+        return Winner.TIE;
     }
 
 
     public void insertCoin(int column, int playerID) {
-        //TODO Write your implementation for this method	
+        gameState[column][getEmptyRowOnColumn(column)] = playerID;
     }
 
     public int decideNextMove() {
-        //TODO Write your implementation for this method
-        return new Random().nextInt(x);
+        Random rdm = new Random();
+        int move;
+        do{
+            move = rdm.nextInt(x);
+        }
+        while(getEmptyRowOnColumn(move) == -1);
+        return move;
     }
 
+
+
+    private int getEmptyRowOnColumn(int column)
+    {
+        for (int i = 0; i<gameState[column].length ;i++)
+        {
+            if(gameState[column][i] == 0) return i;
+        }
+        return -1;
+    }
+
+    private boolean CheckWin(int i, int j, int playerId)
+    {
+        if(j+3<=y) // row
+        {
+            if(gameState[i][j]==playerId && gameState[i][j+1]==playerId && gameState[i][j+2]==playerId && gameState[i][j+3]==playerId)
+            {
+                return true;
+            }
+        }
+        if(i+3<=x) // column
+        {
+            if(gameState[i][j]==playerId && gameState[i+1][j]==playerId && gameState[i+2][j]==playerId && gameState[i+3][j]==playerId)
+            {
+                return true;
+            }
+            if(j+3<=y) // diagonal upwards
+            {
+                if(gameState[i][j]==playerId && gameState[i+1][j+1]==playerId && gameState[i+2][j+2]==playerId && gameState[i+3][j+3]==playerId)
+                {
+                    return true;
+                }
+            }
+            if(j-3>=0) // diagonal downwards
+            {
+                if(gameState[i][j]==playerId && gameState[i+1][j-1]==playerId && gameState[i+2][j-2]==playerId && gameState[i+3][j-3]==playerId)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
